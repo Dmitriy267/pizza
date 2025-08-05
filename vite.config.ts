bd3edf3d3ae2800e73fdc6b08adb9ddf36097ee5
Dmitriy267 <dmitrio87@mail.ru>
2025-08-05 23:44:16 +0300

    fix: исправления в package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/https://github.com/Dmitriy267/pizza',
     plugins: [react()],
 });
