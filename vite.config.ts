b1c89fc563dc07bae889aff6bc9dce5e09faa9af
Dmitriy267 <dmitrio87@mail.ru>
2025-08-06 18:10:18 +0300

    fix: ввел basename

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/',
+    base: process.env.NODE_ENV === 'production' ? '/pizza/' : '/',
     server: {
         port: process.env.PORT as unknown as number,
     },
