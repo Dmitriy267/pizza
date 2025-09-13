35c3411502ecf9a7438ec65817b4eacdddababef
Dmitriy267 <dmitrio87@mail.ru>
2025-09-13 14:58:09 +0300

    fix: исправления в main.tsx и packege.json

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import {
-    createBrowserRouter,
-    RouterProvider,
-    BrowserRouter,
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ErrorPage } from './pages/errorPage/ErrorPage.tsx';
 import { store } from './redux/store.ts';
 import { Provider } from 'react-redux';
@@ -23,103 +19,92 @@ import { PayFinishPage } from './pages/PayFinishPage/PayFinishPage.tsx';
 import { PizzaPage } from './pages/PizzaPage/PizzaPage.tsx';
 import { HomePage } from './pages/HomePage/HomePage.tsx';
 import { Profile } from './pages/Profile/Profile.tsx';
-const router = createBrowserRouter(
-    [
-        {
-            path: '/',
-            element: <App />,
-            children: [
-                {
-                    path: '/',
-                    element: <HomePage />,
-                },
-                {
-                    path: '/Контакты',
-                    element: <Contacts />,
-                },
-                {
-                    path: 'Акции',
-                    element: <ActionsPage />,
-                },
-                {
-                    path: 'Напитки',
-                    element: <DrinksPage />,
-                },
-                {
-                    path: 'Пицца',
-                    element: <PizzaPage />,
-                },
-                {
-                    path: 'Корзина',
-                    element: <ShopPage />,
-                },
-                {
-                    path: 'Войти/Корзина',
-                    element: <ShopPage />,
-                },
-                {
-                    path: 'Пицца/Корзина',
-                    element: <ShopPage />,
-                },
-                {
-                    path: 'Напитки/Корзина',
-                    element: <ShopPage />,
-                },
-                {
-                    path: 'Акции/Корзина',
-                    element: <ShopPage />,
-                },
-                {
-                    path: 'Контакты/Корзина',
-                    element: <ShopPage />,
-                },
-                {
-                    path: 'Войти',
-                    element: <Avtorization />,
-                },
-                {
-                    path: 'Регистрация',
-                    element: <Registration />,
-                },
-                {
-                    path: 'Завершение регистрации',
-                    element: <RegistrationEnd />,
-                },
-                {
-                    path: '/Корзина/Оформление заказа',
-                    element: <OrderPage />,
-                },
-                {
-                    path: '/оплата',
-                    element: <PayPage />,
-                },
-                {
-                    path: '/Завершение заказа',
-                    element: <PayFinishPage />,
-                },
-                {
-                    path: '/Профиль',
-                    element: <Profile />,
-                },
-            ],
-            errorElement: <ErrorPage />,
-        },
-    ],
+const router = createBrowserRouter([
     {
-        basename: '/pizza',
-    }
-);
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <HomePage />,
+            },
+            {
+                path: '/Контакты',
+                element: <Contacts />,
+            },
+            {
+                path: 'Акции',
+                element: <ActionsPage />,
+            },
+            {
+                path: 'Напитки',
+                element: <DrinksPage />,
+            },
+            {
+                path: 'Пицца',
+                element: <PizzaPage />,
+            },
+            {
+                path: 'Корзина',
+                element: <ShopPage />,
+            },
+            {
+                path: 'Войти/Корзина',
+                element: <ShopPage />,
+            },
+            {
+                path: 'Пицца/Корзина',
+                element: <ShopPage />,
+            },
+            {
+                path: 'Напитки/Корзина',
+                element: <ShopPage />,
+            },
+            {
+                path: 'Акции/Корзина',
+                element: <ShopPage />,
+            },
+            {
+                path: 'Контакты/Корзина',
+                element: <ShopPage />,
+            },
+            {
+                path: 'Войти',
+                element: <Avtorization />,
+            },
+            {
+                path: 'Регистрация',
+                element: <Registration />,
+            },
+            {
+                path: 'Завершение регистрации',
+                element: <RegistrationEnd />,
+            },
+            {
+                path: '/Корзина/Оформление заказа',
+                element: <OrderPage />,
+            },
+            {
+                path: '/оплата',
+                element: <PayPage />,
+            },
+            {
+                path: '/Завершение заказа',
+                element: <PayFinishPage />,
+            },
+            {
+                path: '/Профиль',
+                element: <Profile />,
+            },
+        ],
+        errorElement: <ErrorPage />,
+    },
+]);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider store={store}>
-            <BrowserRouter
-                basename={
-                    process.env.NODE_ENV === 'production' ? '/pizza' : '/'
-                }
-            >
-                <RouterProvider router={router} />
-            </BrowserRouter>
+            <RouterProvider router={router} />
         </Provider>
     </StrictMode>
 );
